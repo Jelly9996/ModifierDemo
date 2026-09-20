713d53dee289b20dc7e4869d0382acfc77417ad8
OLGA <ku1460155@gmail.com>
2026-09-20 15:36:08 +0700

    Add border

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -15,6 +15,9 @@ import com.example.modifierdemo.ui.theme.ModifierDemoTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +34,12 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
+    val mymodifier = Modifier
+        .border(width = 2.dp, color = Color.Black)
+        .padding(all = 10.dp)
     Text(
         "Hello Compose",
+        modifier = mymodifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
